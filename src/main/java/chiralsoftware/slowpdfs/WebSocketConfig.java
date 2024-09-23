bb3a560d7819917bd2b1cf2be69c9ef87607b332
Eric <eric@chiralsoftware.com>
2024-09-23 14:38:20 -0700

    added code to show using a token for authentication

--- src/main/java/chiralsoftware/slowpdfs/WebSocketConfig.java
+++ src/main/java/chiralsoftware/slowpdfs/WebSocketConfig.java
@@ -4,9 +4,18 @@ import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static java.util.regex.Pattern.compile;
+import static org.springframework.messaging.support.MessageHeaderAccessor.getAccessor;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -64,5 +73,29 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                final StompHeaderAccessor accessor =
+                        getAccessor(message, StompHeaderAccessor.class);
+                if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String token = accessor.getFirstNativeHeader("Authorization");
+                    if (token != null && token.startsWith("Bearer ")) {
+                        token = token.substring(7);
+                        LOG.info("this is the auth token: " + token);
+                        // in real use we would validate the token and set the authentication
+                        // Validate the token and set the user
+//                        Authentication auth = validateToken(token);
+//                        accessor.setUser(auth);
+//                        SecurityContextHolder.getContext().setAuthentication(auth);
+                    }
+                }
+                return message;
+            }
+        });
     }
 }
--- src/main/webapp/action.js
+++ src/main/webapp/action.js
@@ -37,39 +37,31 @@ function connect() {
     const client = new StompJs.Client({
         webSocketFactory: () => socket,
         connectHeaders: { Authorization: `Bearer my-token` },
-        onConnect: () => { console.log("Connected"); },
+        onConnect: () => {
+            console.log("Connected");
+            client.subscribe('/topic/status', function (messageOutput) {
+                console.log("New Message: " + messageOutput);
+                var messageObject = $.parseJSON(messageOutput.body);
+                $('#fileProgress').val(messageObject.progress);
+                $('#message').text(messageObject.message);
+                if(messageObject.status === "complete") {
+                    console.log("The status is complete, download link should be: download/" + messageObject.filename + ".zip");
+                    $("#results").append("<tr><td>File: " + messageObject.filename + "</td>" +
+                        "<td><a href=\"./download/" + messageObject.filename + ".zip\">" +
+                        messageObject.filename + "</a></td></tr>\n"
+                    );
+                    $('#startButton').prop('disabled', false);
+                    $('#fileProgress').val(0);
+                }
+            });
+
+        },
         onStompError: (frame) => {
             console.error('broker reported error: ' + frame.headers['message']);
+            console.log('Additional details: ' + frame.body);
         }
     });
     client.activate();
-    // fix this - merge the subscribe code into this socket above
-    // and fix the Spring side to change for the auth token
 
-    const stompClient = StompJs.Stomp.over(socket);
-
-    stompClient.onConnect = function (frame) {
-        stompClient.subscribe('/topic/status', function (messageOutput) {
-            console.log("New Message: " + messageOutput);
-            var messageObject = $.parseJSON(messageOutput.body);
-            $('#fileProgress').val(messageObject.progress);
-            $('#message').text(messageObject.message);
-            if(messageObject.status === "complete") {
-                console.log("The status is complete, download link should be: download/" + messageObject.filename + ".zip");
-                $("#results").append("<tr><td>File: " + messageObject.filename + "</td>" + 
-                        "<td><a href=\"./download/" + messageObject.filename + ".zip\">" + 
-                        messageObject.filename + "</a></td></tr>\n"
-                        );
-                $('#startButton').prop('disabled', false);
-                $('#fileProgress').val(0);
-            }
-        });
-
-    };
-    stompClient.onStompError = function(frame) {
-      console.log('Broker reported error: ' + frame.headers['message']);
-      console.log('Additional details: ' + frame.body);
-    }
-    stompClient.activate();
     console.log("Stomp should be connected");
 }
